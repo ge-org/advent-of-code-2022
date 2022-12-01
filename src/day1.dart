43cc5f4cd7f22d11b510e07f87e412a8bf2f309b
Georg Dresler <georg@g-dresler.de>
2022-12-01 13:23:45 +0100

    Day 1 part 2

--- src/day1.dart
+++ src/day1.dart
@@ -5,12 +5,17 @@ import 'support.dart';
 void main() async {
   final testInput = await readInput("day1_sample");
   check(day1Part1(testInput), 24000);
+  check(day1Part2(testInput), 45000);
 
   final input = await readInput("day1");
   print(day1Part1(input));
+  print(day1Part2(input));
 }
 
 int day1Part1(String input) => caloriesPerElve(input).max;
+
+int day1Part2(String input) =>
+    caloriesPerElve(input).sorted((a, b) => b.compareTo(a)).take(3).sum;
 
 Iterable<int> caloriesPerElve(String input) =>
     input.readParagraphs().readIntLines().map((calories) => calories.sum);
