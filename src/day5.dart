430e864a16a9f2549b9bbf1e0b5d2b50fcfec885
Georg Dresler <georg@g-dresler.de>
2022-12-05 19:54:08 +0100

    Day 5 part 2

--- src/day5.dart
+++ src/day5.dart
@@ -4,19 +4,21 @@ import 'support.dart';
 
 void main() async {
   final testInput = await readInput("day5_sample");
-  check(part1(testInput), "CMZ");
+  check(part1(testInput, moveMultiple: false), "CMZ");
+  check(part1(testInput, moveMultiple: true), "MCD");
 
   final input = await readInput("day5");
-  print(part1(input));
+  print(part1(input, moveMultiple: false));
+  print(part1(input, moveMultiple: true));
 }
 
-String part1(String input) {
+String part1(String input, {required bool moveMultiple}) {
   final stackAndCommands = input.readParagraphs();
   final stackInput = stackAndCommands[0].readLines();
   final stack = getStack(stackInput);
   final commandInput = stackAndCommands[1].readLines();
   final commands = getCommands(commandInput);
-  return applyCommands(stack, commands)
+  return applyCommands(stack, commands, moveMultiple)
       .entries
       .toList()
       .sorted((lhs, rhs) => lhs.key.compareTo(rhs.key))
@@ -58,11 +60,15 @@ List<List<int>> getCommands(List<String> commandInput) => commandInput
     .toList();
 
 Map<int, List<String>> applyCommands(
-        Map<int, List<String>> stack, List<List<int>> commands) =>
+  Map<int, List<String>> stack,
+  List<List<int>> commands,
+  bool moveMultiple,
+) =>
     commands.fold(stack, (stack, command) {
       final amount = command[0];
       final itemsToMove = stack[command[1]]!.sublist(0, amount);
       stack[command[1]]!.removeRange(0, amount);
-      stack[command[2]]!.insertAll(0, itemsToMove.reversed);
+      stack[command[2]]!
+          .insertAll(0, moveMultiple ? itemsToMove : itemsToMove.reversed);
       return stack;
     });
