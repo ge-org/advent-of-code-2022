9cda43c07ebbc8455186d53d4669b1891f0c5c8c
Georg Dresler <georg@g-dresler.de>
2022-12-05 17:15:02 +0100

    Day 4 part 2

--- src/day4.dart
+++ src/day4.dart
@@ -5,31 +5,38 @@ import 'support.dart';
 void main() async {
   final testInput = await readInput("day4_sample");
   check(part1(testInput), 2);
+  check(part2(testInput), 4);
 
   final input = await readInput("day4");
   print(part1(input));
+  print(part2(input));
 }
 
-int part1(String input) => input
-        .readLines()
-        .map((line) => line.split(',').map(
-            (sectionRange) => sectionRange.split('-').map((e) => int.parse(e))))
-        .flattened
-        .slices(2)
-        .where((pairs) {
+int part1(String input) => getPairs(input).where((pairs) {
       final pairA = pairs[0].toList();
       final pairB = pairs[1].toList();
-      var aInB = pairA[0] >= pairB[0] &&
-          pairA[0] <= pairB[1] &&
-          pairA[1] >= pairB[0] &&
-          pairA[1] <= pairB[1];
-      var bInA = pairB[0] >= pairA[0] &&
-          pairB[0] <= pairA[1] &&
-          pairB[1] >= pairA[0] &&
-          pairB[1] <= pairA[1];
-      if (aInB || bInA) {
-        return true;
-      } else {
-        return false;
-      }
+      return oneContainedInOther(pairA, pairB);
     }).length;
+
+int part2(String input) => getPairs(input).where((pairs) {
+      final pairA = pairs[0].toList();
+      final pairB = pairs[1].toList();
+      return !oneOverlapsOther(pairA, pairB);
+    }).length;
+
+Iterable<List<Iterable<int>>> getPairs(String input) => input
+    .readLines()
+    .map((line) => line.split(',').map(
+        (sectionRange) => sectionRange.split('-').map((e) => int.parse(e))))
+    .flattened
+    .slices(2);
+
+bool oneContainedInOther(List<int> pairA, List<int> pairB) =>
+    pairA[0] >= pairB[0] && pairA[1] <= pairB[1] ||
+    pairB[0] >= pairA[0] && pairB[1] <= pairA[1];
+
+bool oneOverlapsOther(List<int> pairA, List<int> pairB) =>
+    pairA[1] < pairB[0] ||
+    pairA[0] > pairB[1] ||
+    pairB[1] < pairA[0] ||
+    pairB[0] > pairA[1];
