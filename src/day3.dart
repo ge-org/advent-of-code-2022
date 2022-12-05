0aabf52a232198e1dade99392523bfe5cd5c2eb0
Georg Dresler <georg@g-dresler.de>
2022-12-05 15:24:33 +0100

    Day 3 part 2

--- src/day3.dart
+++ src/day3.dart
@@ -5,21 +5,38 @@ import 'support.dart';
 void main() async {
   final testInput = await readInput("day3_sample");
   check(part1(testInput), 157);
+  check(part2(testInput), 70);
 
   final input = await readInput("day3");
   print(part1(input));
+  print(part2(input));
 }
 
 int part1(String input) => input
     .readLines()
     .map((line) {
       var half = line.length ~/ 2;
-      final compartmentA = line.substring(0, half).split('').toSet();
-      final compartmentB = line.substring(half).split('').toSet();
-      return compartmentA.intersection(compartmentB);
+      final compartmentA = line.substring(0, half).split('');
+      final compartmentB = line.substring(half).split('');
+      return getCommonElements([compartmentA, compartmentB]);
     })
     .map((elements) => elements.map((e) => getPriority(e)).sum)
     .sum;
+
+int part2(String input) => input
+    .readLines()
+    .slices(3)
+    .map((group) => group.map((e) => e.split('')))
+    .map(getCommonElements)
+    .flattened
+    .map((e) => getPriority(e))
+    .sum;
+
+Set<dynamic> getCommonElements(Iterable<Iterable<dynamic>> collections) =>
+    collections.fold(
+        collections.first.toSet(),
+        (commonElements, current) =>
+            commonElements.intersection(current.toSet()));
 
 const priorities = "abcdefghijklmnopqrstyvwxyzABCDEFGHIJKLMNOPQRSTYVWXYZ";
 
