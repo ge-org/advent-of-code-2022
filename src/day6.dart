a0040d9a8c731b5716b50ae6746bcdaaea6cf881
Georg Dresler <georg@g-dresler.de>
2022-12-06 09:47:15 +0100

    Day 6 part 2

--- src/day6.dart
+++ src/day6.dart
@@ -5,20 +5,24 @@ import 'support.dart';
 void main() async {
   final testInput = (await readInput("day6_sample")).readLines();
   ({0: 7, 1: 5, 2: 6, 3: 10, 4: 11}).forEach((index, solution) {
-    check(part1(testInput[index]), solution);
+    check(findMarker(testInput[index], markerLength: 4), solution);
+  });
+  ({0: 19, 1: 23, 2: 23, 3: 29, 4: 26}).forEach((index, solution) {
+    check(findMarker(testInput[index], markerLength: 14), solution);
   });
 
   final input = await readInput("day6");
-  print(part1(input));
+  print(findMarker(input, markerLength: 4));
+  print(findMarker(input, markerLength: 14));
 }
 
-int part1(String input) =>
+int findMarker(String input, {required int markerLength}) =>
     input.split('').foldIndexed(<String, dynamic>{
       "marker": [],
       "index": 0,
     }, (index, data, currentChar) {
-      if (data["marker"]!.toSet().toList().length != 4) {
-        final markers = add(data["marker"]!, currentChar, maxSize: 4);
+      if (data["marker"]!.toSet().toList().length != markerLength) {
+        final markers = add(data["marker"]!, currentChar, markerLength);
         data["marker"] = markers;
         data["index"] = index;
       }
@@ -26,8 +30,7 @@ int part1(String input) =>
     })["index"] +
     1;
 
-List<dynamic> add(List<dynamic> collection, dynamic element,
-    {required int maxSize}) {
+List<dynamic> add(List<dynamic> collection, dynamic element, int maxSize) {
   if (collection.length >= maxSize) {
     return collection
       ..removeAt(0)
